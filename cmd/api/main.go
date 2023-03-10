37fda1ecd75d3fe24be1db47ca0be7c65c6ce028
uyutaka <yutaka.urakami@colorado.edu>
2023-03-10 00:41:57 +0000

    S5 -> 45.Improving routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -34,23 +33,14 @@ func main() {
 	}
 }
 
+// serve starts the web server
 func (app *application) serve() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var payload struct {
-			Okay    bool   `json:"ok"`
-			Message string `json:"message"`
-		}
-		payload.Okay = true
-		payload.Message = "Hello, world"
-		out, err := json.MarshalIndent(payload, "", "\t")
-		if err != nil {
-			app.errorLog.Println(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(out)
-	})
-
 	app.infoLog.Println("API Listening on port", app.config.port)
-	return http.ListenAndServe(fmt.Sprintf(":%d", app.config.port), nil)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+	}
+
+	return srv.ListenAndServe()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/uyutaka/books-go
 
 go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.8
