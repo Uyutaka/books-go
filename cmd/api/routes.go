61d534ed38d4ade84327a9e010cad1d0fe46ce59
uyutaka <yutaka.urakami@colorado.edu>
2023-03-14 00:31:06 +0000

    S7 -> 85.Deleting a user's token on the back end on logout

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,8 +25,8 @@ func (app *application) routes() http.Handler {
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
 
-	mux.Get("/users/login", app.Login)
 	mux.Post("/users/login", app.Login)
+	mux.Post("/users/logout", app.Logout)
 
 	mux.Get("/users/all", func(w http.ResponseWriter, r *http.Request) {
 		var users data.User
