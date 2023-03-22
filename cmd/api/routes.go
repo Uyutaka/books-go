4e5892e8a0b6261ca8c6f0674d60331c0802da42
uyutaka <yutaka.urakami@colorado.edu>
2023-03-22 07:45:46 +0000

    S9 -> 120.Getting started with listing all books

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,8 +25,11 @@ func (app *application) routes() http.Handler {
 	mux.Post("/users/login", app.Login)
 	mux.Post("/users/logout", app.Logout)
 
+	mux.Post("/books", app.AllBooks)
+	mux.Get("/books", app.AllBooks)
+
 	mux.Post("/validate-token", app.ValidateToken)
-	
+
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.AuthTokenMiddleware)
 
@@ -37,5 +40,9 @@ func (app *application) routes() http.Handler {
 		mux.Post("/log-user-out/{id}", app.LogUserOutAndSetInactive)
 	})
 
+	// static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	
 	return mux
 }
