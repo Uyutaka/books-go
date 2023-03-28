9ceebc93e32a7431ead535a6263995c0eaa4f410
uyutaka <yutaka.urakami@colorado.edu>
2023-03-28 05:37:14 +0000

    S9 -> 136.Deleting a book

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,18 +8,20 @@ import (
 	"github.com/go-chi/cors"
 )
 
+// routes generates our routes and attaches them to handlers, using the chi router
+// note that we return type http.Handler, and not *chi.Mux; since chi.Mux satisfies
+// the interface requirements for http.Handler, it makes sense to return the type
+// that is part of the standard library.
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		AllowCredentials: true,
+		MaxAge:           300,
 	}))
 
 	mux.Post("/users/login", app.Login)
@@ -31,9 +33,13 @@ func (app *application) routes() http.Handler {
 
 	mux.Post("/validate-token", app.ValidateToken)
 
+	// all of the routes in the block below are prefixed with /admin, and also
+	// require that the user have a valid token provided in the request, since
+	// this block uses the AuthTokenMiddleware
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.AuthTokenMiddleware)
 
+		// admin user routes
 		mux.Post("/users", app.AllUsers)
 		mux.Post("/users/save", app.EditUser)
 		mux.Post("/users/get/{id}", app.GetUser)
@@ -43,6 +49,9 @@ func (app *application) routes() http.Handler {
 		// admin book routes
 		mux.Post("/authors/all", app.AuthorsAll)
 		mux.Post("/books/save", app.EditBook)
+		mux.Post("/books/delete", app.DeleteBook)
+		mux.Post("/books/{id}", app.BookByID)
+
 	})
 
 	// static files
